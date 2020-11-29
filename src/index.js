29a065a72dd40410bb500edbd92ab2e616b74c7d
Wagner de Oliveira Paz <wagner.o.paz@gmail.com>
2020-11-28 23:08:52 -0300

    Centered full screen bunny

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import * as PIXI from 'pixi.js';
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
-const app = new PIXI.Application();
+const app = new PIXI.Application({transparent: true});
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+
+app.renderer.resize(window.innerWidth, window.innerHeight);
+window.onresize = function()
+{
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+}
 
 // The application will create a canvas element for you that you
 // can then insert into the DOM
@@ -30,4 +39,11 @@ app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
          // each frame we spin the bunny around a bit
         bunny.rotation += 0.01;
     });
+});
+
+app.renderer.on('resize', () => {
+    console.log('resizing...');
+    const bunny = app.stage.getChildAt(0);
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
 });
