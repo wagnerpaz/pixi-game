b786b975b3e14986dea479be00b2ec20b4efd61e
Wagner de Oliveira Paz <wagner.o.paz@gmail.com>
2021-02-11 09:43:48 -0300

    Collision detection

--- src/anims/heroAnim.js
+++ src/anims/heroAnim.js
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js';
 
-export default (app) => {
+export default (PIXI, app, container) => {
     return new Promise(res => {
         app.loader.add('hero.json').load((loader, resources) => {
             const sheet = resources['hero.json'];
@@ -8,41 +8,50 @@ export default (app) => {
             const anims = [];
             const addAnim = (name) => {
                 const anim = new PIXI.AnimatedSprite(sheet.spritesheet.animations[name]);
-                setAnchor(anim);
-                anim.renderable = false;
                 anims.push(anim);
                 return anim;
             };
             const invalidateAnims = (anim) => {
                 anims.forEach(a => {
-                    if(a !== anim) {
-                        a.stop();
-                        a.renderable = false;
-                    }
+                    a.stop();
+                    container.removeChildren();
                 });
             };
             const validateAnim = (anim) => {
                 anim.play();
-                anim.renderable = true;
+                setAnchor(anim);
+                setAnchor(anim.boundingBox, anim);
+                container.addChild(anim);
+                container.addChild(anim.boundingBox);
             };
             const run = (anim) => {
                 invalidateAnims(anim);
                 validateAnim(anim);
+                return anim;
             }
     
             const heroStanding = addAnim("hero_standing");
             heroStanding.animationSpeed = 0.025;
+            heroStanding.boundingBox = new PIXI.Graphics();
+            heroStanding.boundingBox.visible = false;
+            heroStanding.boundingBox.lineStyle(1, 0xFF0000);
+            heroStanding.boundingBox.drawRect(3, 1, 10, 15);
+
             
             const heroWalking = addAnim("hero_walking");
             heroWalking.animationSpeed = 0.1; 
+            heroWalking.boundingBox = new PIXI.Graphics();
+            heroWalking.boundingBox.visible = false;
+            heroWalking.boundingBox.lineStyle(1, 0xFF0000);
+            heroWalking.boundingBox.drawRect(3, 1, 10, 15);
     
             const result = {
                 anims,
                 walk: () => {
-                    run(heroWalking);
+                    return run(heroWalking);
                 },
                 stand: () => {
-                    run(heroStanding);
+                    return run(heroStanding);
                 }
             }
 
@@ -51,7 +60,6 @@ export default (app) => {
     });
 };
 
-function setAnchor(sprite) {
-    sprite.anchor.x = 0.5;
-    sprite.anchor.y = 1;
+function setAnchor(set, ref) {
+    set.pivot.set((ref||set).width / 2, 0);
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import * as PIXI from 'pixi.js';
 import nipplejs from 'nipplejs';
+import Bump from 'bump.js';
+import SAT from 'sat';
 
 import heroAnim from './anims/heroAnim';
 import keyboard from './utils/keyboard';
 
 PIXI.settings.RESOLUTION = 5;
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+const b = new Bump(PIXI);
 
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
@@ -14,7 +18,7 @@ const app = new PIXI.Application({transparent: true});
 
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
-app.renderer.view.style.backgroundColor = "#00FF00";
+app.renderer.view.style.backgroundColor = "#66CCFF";
 
 window.onresize = function()
 {
@@ -28,13 +32,13 @@ document.body.appendChild(app.view);
 
 const nippleManager = nipplejs.create({zone: document.getElementById('controls-overlay'), color: '#333', maxNumberOfNipples: 1});
 
-heroAnim(app).then(heroAnim => {
-    const hero = new PIXI.Container();
+const hero = new PIXI.Container();
+
+heroAnim(PIXI, app, hero).then(heroAnim => {
     hero.vx = 0;
     hero.vy = 0;
-    hero.addChild(...heroAnim.anims);
     hero.x = app.renderer.width / 2 / PIXI.settings.RESOLUTION;
-    hero.y = app.renderer.height / 2 / PIXI.settings.RESOLUTION;
+    hero.y = app.renderer.height / 2 / PIXI.settings.RESOLUTION - hero.height;
     heroAnim.stand();
     
     app.stage.addChild(hero);
@@ -80,13 +84,94 @@ heroAnim(app).then(heroAnim => {
     moveRight.release = () => {
         hero.vx = 0;
         heroAnim.stand();
-    }
+    };
+
+    const moveUp = keyboard("ArrowUp");
+    moveUp.press = () => {
+        hero.vy = -1;
+        heroAnim.walk();
+    };
+    moveUp.release = () => {
+        hero.vy = 0;
+        heroAnim.stand();
+    };
+
+    const moveDown = keyboard("ArrowDown");
+    moveDown.press = () => {
+        hero.vy = 1;
+        heroAnim.walk();
+    };
+    moveDown.release = () => {
+        hero.vy = 0;
+        heroAnim.stand();
+    };
+
+    const floor = new PIXI.Graphics();
+    floor.beginFill(0x00FF00);
+    floor.drawRect(0, 0, app.view.width / 1.5 / PIXI.settings.RESOLUTION, app.view.height/ 1.5 / PIXI.settings.RESOLUTION);
+    floor.endFill();
+    floor.x = 0;
+    floor.y = app.renderer.height / 1.5 / PIXI.settings.RESOLUTION;
+    app.stage.addChild(floor);
+
+    const wall1 = new PIXI.Graphics();
+    wall1.beginFill(0x00FF00);
+    wall1.drawRect(0, 0, 10, app.view.height / PIXI.settings.RESOLUTION);
+    wall1.endFill();
+    wall1.x = 0;
+    wall1.y = 0;
+    app.stage.addChild(wall1);
 
     app.ticker.add(delta => {
-        hero.x += hero.vx * delta;
-        hero.y += hero.vy * delta;
+        const GRAVITY = 0.05;
+        
+        hero.vy += GRAVITY;
+        const xu = hero.vx * delta;
+        const yu = hero.vy * delta;
+
+        const heroNewPosX = parentPositionRef(hero.getChildAt(0).boundingBox, xu, 0);
+        if(hitTestRectangle(heroNewPosX, floor) && hero.vx !== 0) {
+            hero.vx = 0;
+        }
+        else {
+            hero.x += xu;
+        }
+        
+        const heroNewPosY = parentPositionRef(hero.getChildAt(0).boundingBox, 0, yu);
+        if(hitTestRectangle(heroNewPosY, floor) && hero.vy !== 0) {
+            hero.vy = 0;
+        }
+        else {
+            hero.y += yu;
+        }
     });
 });
+
+function parentPositionRef(child, xu, yu) {
+    const parent = child.parent;
+    const clone = child.clone();
+
+    clone.x = parent.x + child.x;
+    clone.y = parent.y + child.y;
+
+    clone.x -= child.pivot.x / 2;
+    clone.y -= child.pivot.y / 2;
+    
+    clone.x += xu;
+    clone.y += yu;
+
+    return clone;
+}
+
+function hitTestRectangle(obj1, obj2) {
+    const box1 = toSatBoxPolygon(obj1);
+    const box2 = toSatBoxPolygon(obj2);
+    return SAT.testPolygonPolygon(box1, box2);
+}
+
+function toSatBoxPolygon(obj) {
+    return new SAT.Box(new SAT.Vector(obj.x, obj.y), obj.width, obj.height).toPolygon();
+}
 
 app.renderer.on('resize', () => {
     const bunny = app.stage.getChildAt(0);
